c737659769deddfea98649eb37f4568d095b3993
nayvcake <43297329+nayvcake@users.noreply.github.com>
2022-07-24 15:11:13 -0300

    fix(index): fixing index.ts

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,6 +1,34 @@
 export const Models = {
-  Channel: import('./models/channels/channel'),
-  ChannelInvite: import('./models/channels/channelInvite'),
-  ChannelUnread: import('./models/channels/channelUnread'),
-  Message: import('./models/channels/message')
+  Channels: {
+    Channel: import('./models/channels/channel'),
+    ChannelInvite: import('./models/channels/channelInvite'),
+    ChannelUnread: import('./models/channels/channelUnread'),
+    Message: import('./models/channels/message'),
+  },
+
+  Media: {
+    Attachment: import('./models/media/attachment'),
+    Emoji: import('./models/media/emoji'),
+  },
+
+  Servers: {
+    Server: import('./models/servers/server'),
+    Emoji: import('./models/servers/serverMember'),
+  },
+
+  Users: {
+    Bot: import('./models/users/bot'),
+    User: import('./models/users/user'),
+  },
+
+}
+
+export const Permissions = {
+  Permission: import('./permissions/permission'),
+}
+
+export const Embeds = {
+  Embed: import('./embeds/embed'),
+  Media: import('./embeds/media'),
+  Special: import('./embeds/special'),
 }
