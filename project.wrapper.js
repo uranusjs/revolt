5b15865c13c91c0900615ff405d9909a7f9e0754
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-08-27 20:53:14 -0300

    Clean cache

--- project.wrapper.js
+++ project.wrapper.js
@@ -4,72 +4,83 @@ const ProjectWrapper = require('tsc-compile-projects')
 const Events = require('tsc-compile-projects/src/events').Event
 const devMode = false
 const projects = [
-  {
-    'name': 'base',
-    'projectDir': './packages/base',
-    'watchMode': devMode,
-    'args': []
-  },
-  {
-    'name': 'models',
-    'projectDir': './packages/models',
-    'watchMode': devMode,
-    'args': []
-  },
-  {
-    'name': 'rest',
-    'projectDir': './packages/rest',
-    'watchMode': devMode,
-    'args': []
-  },
-  {
-    'name': 'utils',
-    'projectDir': './packages/utils',
-    'watchMode': devMode,
-    'args': []
-  },
-  {
-    'name': 'websocket',
-    'projectDir': './packages/websocket',
-    'watchMode': devMode,
-    'args': []
-  }
+    {
+        'name': 'base',
+        'projectDir': './packages/base',
+        'watchMode': devMode,
+        'provider': 'yarn',
+        'installPackage': true,
+        'autoUpdatePackage': false,
+        'args': []
+    },
+    {
+        'name': 'models',
+        'projectDir': './packages/models',
+        'watchMode': devMode,
+        'installPackage': true,
+        'autoUpdatePackage': false,
+        'args': []
+    },
+    {
+        'name': 'rest',
+        'projectDir': './packages/rest',
+        'watchMode': devMode,
+        'installPackage': true,
+        'autoUpdatePackage': false,
+        'args': []
+    },
+    {
+        'name': 'utils',
+        'projectDir': './packages/utils',
+        'watchMode': devMode,
+        'installPackage': true,
+        'autoUpdatePackage': false,
+        'args': []
+    },
+    {
+        'name': 'websocket',
+        'projectDir': './packages/websocket',
+        'watchMode': devMode,
+        'installPackage': true,
+        'autoUpdatePackage': false,
+        'args': []
+    }
 ]
 
 console.log('Starting for compile packages!')
 if (devMode) {
-  console.log(`Loading ${projects.length} packages...`)
+    console.log(`Loading ${projects.length} packages...`)
 } else {
-  console.log(`Compiling ${projects.length} packages...`)
+    console.log(`Compiling ${projects.length} packages...`)
 }
 
 class Wrapper {
-  static runner() {
-    const building = ProjectWrapper.initializeDefault({
-      hotReload: devMode,
-      command: [],
-      lowCpuUsage: true,
-      targets: projects
-    })
+    static runner() {
+        const building = ProjectWrapper.initializeDefault({
+            "hotReload": devMode,
+            "command": [],
+            "lowCpuUsage": true,
+            "targets": projects
+        })
 
-    building.then((buildBase) => {
-      buildBase.on('endCompile', () => {
-        console.log(`\n\n\n\n  >  :@uranusjs/typescript has been compiled successfully!`)
-      })
-      buildBase.interpreter.on('debugData', ({ eventName, interpreter, parse }) => {
-        switch (eventName) {
-          case Events.STARTING_COMPILATION:
-            console.log(` > Typescript  ::Compiling -> ${interpreter.projectName}`)
-            break
-          case Events.ERROR_TS:
-            console.log(`  | Typescript::${interpreter.projectName} > Error -> ${parse.metadata.messageOriginal}`)
-            break
-          default:
-            break
-        }
-      })
-    })
-  }
+        building.then((buildBase) => {
+            buildBase.on('endCompile', () => {
+                console.log(`\n\n\n\n  >  :@uranusjs/typescript has been compiled successfully!`)
+            })
+            buildBase.interpreter.on('debugData', ({ eventName, interpreter, parse }) => {
+                switch (eventName) {
+                case Events.STARTING_COMPILATION:
+                    console.log(` > Typescript  ::Compiling -> ${interpreter.projectName}`)
+                    break
+                case Events.ERROR_TS:
+                    console.log(`  | Typescript::${interpreter.projectName} > Error -> ${parse.metadata.messageOriginal}`)
+                    break
+                default:
+                    break
+                }
+            })
+        })
+    }
 }
 
 
