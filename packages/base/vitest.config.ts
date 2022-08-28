eb07cdaccd4c20c34114643b60aa8020e98bb73a
Nayv <43297329+nayvcake@users.noreply.github.com>
2022-08-27 21:44:51 -0300

    Point vitest to test folder

--- packages/base/vitest.config.ts
+++ packages/base/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     "test": {
-        "include": ['**/test/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', '**/src/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        "include": ['**/test/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', '**/test/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
         "exclude": ['**/node_modules/**', '**/dist/**', 'build', '**/cypress/**', '**/.{idea,git,cache,output,temp}/**'],
         "watchExclude": ['**/node_modules/**', '**/dist/**', 'build'],
     },
